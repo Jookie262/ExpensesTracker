c7de68df80fd304c7064088b49266a2d228a9207
JolouTheRookie <barrientos.134038@iloilo.sti.edu.ph>
2020-11-22 21:11:14 +0800

    Readable Date and Time Fix

--- expenses.py
+++ expenses.py
@@ -5,37 +5,45 @@
 import csv
 import os.path
 import texttable
+import datetime
 # ==================== Create Method Here ============================#
 
 # This method is use to ask user about the information
 def save_items(): 
+    # Automatically inputs time based on the users computer
+    auto_datetime = str(datetime.datetime.now())
     # Ask user for an item that you have spent
     input_name = input("Item that I spent money today: ")
     # Ask user for the price of the item
     input_price = input("How much did I spent? ₱")
     # Returns name and price of the item
-    return input_name, input_price
+    return auto_datetime, input_name, input_price
 
 # This method is used to store data in the csv file
-def add_spent(name, ammount): 
+def add_spent(date, name, ammount): 
     with open("data/items.csv", "a") as save_file:
-        save_file.write('\n'+ name + ',' + ammount)
+        save_file.write('\n' + date + ',' + name + ',' + ammount)
 
 # This method prints the CSV data in table view
 def expenses_summary():
     # using texttable pip
-    val = ['','']
+    val = ['','','']
     dic = csv.DictReader(open("data/items.csv"))
     for data in dic:
-        val[0] += data['name'] + "\n"
-        val[1] += data['price'] + "\n"
+        cr_date = datetime.datetime.strptime(data['datetime'], '%Y-%m-%d %H:%M:%S.%f')
+        cr_date = cr_date.strftime("%B %d %Y")
+        val[0] += cr_date + "\n"
+        val[1] += data['name'] + "\n"
+        val[2] += data['price'] + "\n"
+
+    print(dic)
 
     table = texttable.Texttable()
     table.add_rows(
                 [
-                ['Name', 'Price'],
-                [val[0], val[1]],
-                ['Total Expenses', total_expenses()]
+                ['Date', 'Name', 'Price'],
+                [val[0], val[1], val[2]],
+                ['Total Expenses', '', total_expenses()]
                 ])
     return(table.draw())
 
@@ -54,7 +62,7 @@ def check_CSV():
     if not os.path.exists('data/items.csv'):
         #create a cvs for items
         save_file = open('data/items.csv','a')
-        save_file.write('name' + ',' + 'price' + '\n')
+        save_file.write('datetime' + ',' + 'name' + ',' + 'price' + '\n')
         save_file.close()
 
 
@@ -69,9 +77,9 @@ def main():
     options = input("> ")
     
     if(options == '1'):
-        name, ammount = save_items()
+        date, name, ammount = save_items()
         # Calling out the function add_spent
-        add_spent(name, ammount)
+        add_spent(date, name, ammount)
         main()
     elif(options == '2'):
         # Printin the result in the expenses_summary method
@@ -86,3 +94,5 @@ check_CSV()
 
 # call the main function
 main()
+
+#Testing codes here
